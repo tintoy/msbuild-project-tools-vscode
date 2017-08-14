0841cd29fd725e69f9be0f61c6d154dc3da5e999
tintoy <tintoy@tintoy.io>
2017-08-14 13:59:26 +1000

    Add language server (currently borked due to JsonRpc reference incompatibility).

---
+++ src/LanguageServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using Lsp;
+using Lsp.Models;
+using Serilog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MSBuildProjectTools.LanguageServer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            SynchronizationContext.SetSynchronizationContext(
+                new SynchronizationContext()
+            );
+
+            ConfigureLogging();
+
+            try
+            {
+                AsyncMain(args).Wait();
+            }
+            catch (AggregateException aggregateError)
+            {
+                foreach (Exception unexpectedError in aggregateError.InnerExceptions)
+                {
+                    Log.Error(unexpectedError, "Unexpected error: {Message}", unexpectedError.Message);
+                }
+            }
+            catch (Exception unexpectedError)
+            {
+                Log.Error(unexpectedError, "Unexpected error: {Message}", unexpectedError.Message);
+            }
+        }
+
+        static async Task AsyncMain(string[] args)
+        {
+            var server = new Lsp.LanguageServer(Console.OpenStandardInput(), Console.OpenStandardOutput());
+
+            await server.Initialize();
+            await server.WasShutDown;
+        }
+
+        static void ConfigureLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.LiterateConsole()
+                .CreateLogger();
+        }
+    }
+}

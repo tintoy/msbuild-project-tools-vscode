14463ff0f91ee8ed4f589cbd213db4a61aebb1ae
Adam Friedman <tintoy@tintoy.io>
2017-11-20 04:53:55 +1100

    Use language server from msbuild-project-tools-server.
    tintoy/msbuild-project-tools-vscode#24

--- Build-LanguageServer.ps1
+++ Build-LanguageServer.ps1
@@ -1,4 +1,8 @@
 ﻿$dotnet = Get-Command 'dotnet'
 
-& $dotnet publish "$PSScriptRoot\src\LanguageServer\LanguageServer.csproj" -f netcoreapp2.0 -o "$PSScriptRoot\out\language-server"
-& $dotnet publish "$PSScriptRoot\src\LanguageServer.TaskReflection\LanguageServer.TaskReflection.csproj" -f netcoreapp2.0 -o "$PSScriptRoot\out\task-reflection"
+$serverRoot = Join-Path $PSScriptRoot 'lib\server'
+$publishRoot = Join-Path $PSScriptRoot 'out'
+
+& $dotnet restore "$serverRoot\MSBuildProjectTools.sln"
+& $dotnet publish "$serverRoot\src\LanguageServer\LanguageServer.csproj" -f netcoreapp2.0 -o "$publishRoot\language-server"
+& $dotnet publish "$serverRoot\src\LanguageServer.TaskReflection\LanguageServer.TaskReflection.csproj" -f netcoreapp2.0 -o "$publishRoot\task-reflection"
--- src/LanguageServer.SemanticModel.MSBuild/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("MSBuildProjectTools.LanguageServer.Engine.Tests")]

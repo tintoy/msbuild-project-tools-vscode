e2bc59fbc29430c57853d92cb9a315581c3479a2
Adam Friedman <tintoy@tintoy.io>
2018-01-08 20:18:31 +1100

    Add Linux / MacOS build script for language server.

---
+++ build-language-server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eo pipefail
+
+VersionPrefix="$1"
+VersionSuffix="$2"
+
+if [[ ! $VersionPrefix ]]; then
+    echo 'Must specify a version prefix.'
+
+    exit 1
+fi
+
+ServerRoot="$PWD/lib/server"
+PublishRoot="$PWD/out"
+
+dotnet restore "$ServerRoot/MSBuildProjectTools.sln" /p:VersionPrefix="$VersionPrefix" /p:VersionSuffix="$VersionSuffix"
+dotnet publish "$ServerRoot/src/LanguageServer/LanguageServer.csproj" -f netcoreapp2.0 -o "$PublishRoot/language-server" /p:VersionPrefix="$VersionPrefix" /p:VersionSuffix="$VersionSuffix"
+dotnet publish "$ServerRoot/src/LanguageServer.TaskReflection/LanguageServer.TaskReflection.csproj" -f netcoreapp2.0 -o "$PublishRoot/task-reflection" /p:VersionPrefix="$VersionPrefix" /p:VersionSuffix="$VersionSuffix"

e14287317af542aa88ee1d86fda8a6a3ba86770e
DoctorKrolic <mapmyp03@gmail.com>
2023-08-26 08:43:23 +0300

    Update langusge server submodule and remove task reflection from build scripts

--- Build-LanguageServer.ps1
+++ Build-LanguageServer.ps1
@@ -13,4 +13,3 @@ $publishRoot = Join-Path $PSScriptRoot 'out'
 
 & $dotnet restore "$serverRoot\MSBuildProjectTools.sln" /p:VersionPrefix="$VersionPrefix" /p:VersionSuffix="$($VersionSuffix)"
 & $dotnet publish "$serverRoot\src\LanguageServer\LanguageServer.csproj" -o "$publishRoot\language-server" /p:VersionPrefix="$VersionPrefix" /p:VersionSuffix="$($VersionSuffix)"
-& $dotnet publish "$serverRoot\src\LanguageServer.TaskReflection\LanguageServer.TaskReflection.csproj" -o "$publishRoot\task-reflection" /p:VersionPrefix="$VersionPrefix" /p:VersionSuffix="$($VersionSuffix)"

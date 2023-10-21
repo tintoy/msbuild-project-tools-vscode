0b3c736a80bb596d46ae8ca70f51d74ad7147b4f
DoctorKrolic <mapmyp03@gmail.com>
2023-10-21 22:39:56 +0300

    Add version information

--- Publish-LanguageServer.ps1
+++ Publish-LanguageServer.ps1
@@ -1,11 +1,39 @@
 $dotnet = Get-Command 'dotnet'
 
+$latestTag = git describe --tags --abbrev=0
+
+if ($latestTag.StartsWith('v')) {
+    $latestTag = $latestTag.Substring(1)
+}
+
+$parts = $latestTag.Split('-')
+
+$versionPrefix = $parts[0]
+
+if ($parts.Count -gt 1) {
+    $versionSuffix = $parts[1]
+}
+
 if ($args -contains '--dev') {
     $buildConfiguration = 'Debug'
-    $versionSuffix = 'dev'
+    if ($versionSuffix -eq '') {
+        $versionSuffix = "$versionSuffix-"
+    }
+    $versionSuffix = $versionSuffix + 'dev'
 } else {
     $buildConfiguration = 'Release'
 }
+
+$latestTagFull = git describe --tags
+$latestTagShort = git describe --tags --abbrev=0
+
+if ($latestTagFull -ceq $latestTagShort) {
+    $numberOfCommits = 1
+} else {
+    $numberOfCommits = $latestTagFull.Split('-')[1]
+}
+
+$fileVersion = "$versionPrefix.$numberOfCommits"
 
 Write-Output "Chosen build configuration: $buildConfiguration"
 
@@ -13,4 +41,4 @@ $serverRoot = Join-Path $PSScriptRoot 'lib\server'
 $publishRoot = Join-Path $PSScriptRoot 'out'
 
 & $dotnet restore "$serverRoot\MSBuildProjectTools.sln"
-& $dotnet publish "$serverRoot\src\LanguageServer\LanguageServer.csproj" -o "$publishRoot\language-server" /p:VersionSuffix="$versionSuffix" -c $buildConfiguration
+& $dotnet publish "$serverRoot\src\LanguageServer\LanguageServer.csproj" -o "$publishRoot\language-server" /p:VersionPrefix="$versionPrefix" /p:VersionSuffix="$versionSuffix" /p:FileVersion="$fileVersion" -c $buildConfiguration

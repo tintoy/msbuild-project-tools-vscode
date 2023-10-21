c75599de8efc3e91205837d6b9d0afee023dc57b
DoctorKrolic <mapmyp03@gmail.com>
2023-10-21 17:37:56 +0300

    Add `dev` version suffix when building in dev mode

--- Publish-LanguageServer.ps1
+++ Publish-LanguageServer.ps1
@@ -2,6 +2,7 @@ $dotnet = Get-Command 'dotnet'
 
 if ($args -contains '--dev') {
     $buildConfiguration = 'Debug'
+    $versionSuffix = 'dev'
 } else {
     $buildConfiguration = 'Release'
 }
@@ -12,4 +13,4 @@ $serverRoot = Join-Path $PSScriptRoot 'lib\server'
 $publishRoot = Join-Path $PSScriptRoot 'out'
 
 & $dotnet restore "$serverRoot\MSBuildProjectTools.sln"
-& $dotnet publish "$serverRoot\src\LanguageServer\LanguageServer.csproj" -o "$publishRoot\language-server" -c $buildConfiguration
+& $dotnet publish "$serverRoot\src\LanguageServer\LanguageServer.csproj" -o "$publishRoot\language-server" /p:VersionSuffix="$versionSuffix" -c $buildConfiguration

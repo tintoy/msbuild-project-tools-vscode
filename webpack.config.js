4240c64033fedc7c7821a32b28d6091b8c8d9e1f
DoctorKrolic <mapmyp03@gmail.com>
2023-11-11 13:32:33 +0300

    Pack extension with webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/extension/extension.ts',
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'extension.js',
+        libraryTarget: 'commonjs2',
+        devtoolModuleFilenameTemplate: '../[resource-path]'
+    },
+    devtool: 'source-map',
+    externals: {
+        vscode: 'commonjs vscode'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    node: {
+        __dirname: false
+    },
+    module: {
+        rules: [
+            {
+               test: /\.ts$/,
+               exclude: /node_modules/,
+               use: [
+                   {
+                       loader: 'ts-loader'
+                   }
+               ]
+            }
+        ]
+    }
+};
